b9a2677c819e0e157845f1cb56756ae49b091fc4
moba1 <moba2@protonmail.com>
2025-01-14 13:43:00 +0900

    カードの情報を整理するレイアウトを導入

--- src/components/cards/NormalCard.tsx
+++ src/components/cards/NormalCard.tsx
@@ -7,15 +7,31 @@ export type NormalCardProps = {
   cardImageSource: string;
 };
 
+type CardContentProps = {
+  title: string;
+}
+const CardContentItem = ({ title, children }: react.PropsWithChildren<CardContentProps>) => (
+  <div className='flex space-x-14 align-middle'>
+    <h1>{title}</h1>
+    {children}
+  </div>
+);
+
 const NormalCard = ({ title, keywords, cardImageSource }: NormalCardProps) => (
-  <div>
+  <div className='space-y-6'>
     <Title>{title}</Title>
-    <img src={cardImageSource} alt='card image' />
-    {(keywords ?? []).length > 0 && (
+    <div className='flex space-x-28'>
+      <img src={cardImageSource} alt='card image' />
       <div>
-        {keywords}
+        <CardContentItem title='Keyword'>
+          {(keywords ?? []).length > 0 && (
+            <div>
+              {keywords}
+            </div>
+          )}
+        </CardContentItem>
       </div>
-    )}
+    </div>
   </div>
 );
 
