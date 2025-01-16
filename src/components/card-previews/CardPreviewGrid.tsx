8aa80cd1f59f5ec9f884434a12f212518e0a6a8d
moba1 <moba2@protonmail.com>
2025-01-17 01:11:31 +0900

    カードプレビューグリッドのカラム数を調整

--- src/components/card-previews/CardPreviewGrid.tsx
+++ src/components/card-previews/CardPreviewGrid.tsx
@@ -3,7 +3,7 @@ import * as react from 'react';
 export type CardPreviewGridProps = react.PropsWithChildren<{}>;
 const CardPreviewGrid = ({ children }: CardPreviewGridProps) => (
   <div className="mx-[max(10vw,20px)] my-14">
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
+    <div className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-5 gap-4 justify-center">
       {children}
     </div>
   </div>
