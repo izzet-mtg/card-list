c7b2460c408844f69e6f80ec138c2272a8a6af5c
moba1 <moba2@protonmail.com>
2025-01-17 00:51:26 +0900

    タグのテキストサイズを画像に合わせてモバイルだと少し小さくなるように表示するようにした

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -6,7 +6,7 @@ export type TagProps = {
 
 const Tag = ({ children, backgroundColor, textColor }: TagProps) => {
   return (
-    <div className={`rounded-full px-3 py-1 text-sm w-fit ${backgroundColor} ${textColor}`}>{children}</div>
+    <div className={`rounded-full px-3 py-1 text-xs lg:text-sm w-fit ${backgroundColor} ${textColor}`}>{children}</div>
   );
 };
 
