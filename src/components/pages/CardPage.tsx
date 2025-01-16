f390d788af5d984b150a4aff98f4ef0fd3058af5
moba1 <moba2@protonmail.com>
2025-01-17 01:25:52 +0900

    各ページのレイアウトを調整

--- src/components/pages/CardPage.tsx
+++ src/components/pages/CardPage.tsx
@@ -2,7 +2,7 @@ import * as react from 'react';
 
 export type CardPageProps = react.PropsWithChildren<{}>;
 const CardPage = ({ children }: CardPageProps) => (
-  <div className='max-w-screen-2xl mx-[max(20vw,20px)] my-14'>
+  <div className='mx-[20vw] my-14'>
     {children}
   </div>
 );
--- src/components/pages/CardPreviewGridPage.tsx
+++ src/components/pages/CardPreviewGridPage.tsx
@@ -2,7 +2,7 @@ import * as react from 'react';
 
 export type CardPreviewGridPageProps = react.PropsWithChildren<{}>;
 const CardPreviewGrid = ({ children }: CardPreviewGridPageProps) => (
-  <div className="mx-[max(10vw,20px)] my-14">
+  <div className="mx-[10vw] my-14">
     {children}
   </div>
 );
