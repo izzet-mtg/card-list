3b051affedb6e9b6ed1c8520831960831bf7f84e
moba1 <moba2@protonmail.com>
2025-01-17 15:20:53 +0900

    カードグリッドで表示するカード画像につけていた使用しない class を削除

--- src/components/card-previews/CardPreview.tsx
+++ src/components/card-previews/CardPreview.tsx
@@ -5,7 +5,7 @@ export type CardPreviewProps = {
 const CardPreview = ({ imageUrl, href }: CardPreviewProps) => (
   <a href={href}>
     <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <img className="w-full h-[80%] object-cover object-top" src={imageUrl} alt="card image" />
+      <img src={imageUrl} alt="card image" />
     </div>
   </a>
 );
